c8aa9f6e69bf0fcfac5b6af1a5ada66b27a238f3
MahmoudShaaban00 <ms3198@fayoum.edu.eg>
2026-03-01 01:43:26 +0200

    vercel deploy

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import serverless from 'serverless-http';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server),
+  );
+  await app.init();
 }
+
 bootstrap();
+
+export const handler = serverless(server);
