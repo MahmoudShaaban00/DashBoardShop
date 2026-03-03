839050f5d947a7ade9673d11e7faf0b31978210a
MahmoudShaaban00 <ms3198@fayoum.edu.eg>
2026-03-03 09:56:06 +0200

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from '@modules/auth/auth.module';
+import { AttendanceModule } from '@modules/attendance/attendance.module';
+import { EmployeesModule } from '@modules/employees/employee.module';
+import { ProductModule } from '@modules/product/product.module';
+import { SalesModule } from '@modules/sales/sales.module';
 
 @Module({
   imports: [
@@ -14,6 +19,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         uri: configService.get<string>('MONGO_URI'),
       }),
     }),
+
+    AuthModule,AttendanceModule,EmployeesModule,
+    ProductModule,SalesModule
   ],
 })
 export class AppModule {}
