2a3fbdb9e3c968238ebc078f8c359838df86cbac
Mahmoud shaaban <147880225+MahmoudShaaban00@users.noreply.github.com>
2026-03-05 01:57:02 +0200

    Setup NestJS with Express and configure CORS
    Integrate Express with NestJS and enable CORS for frontend requests.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import  express from 'express';
+import  serverless from 'serverless-http';
+
+const expressApp = express();
+const adapter = new ExpressAdapter(expressApp);
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, adapter);
+
+  // تفعيل CORS عشان الـ frontend يقدر يطلب من الـ backend
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://your-frontend.vercel.app'], 
+    credentials: true,
+  });
+
+  await app.init();
 }
+
 bootstrap();
+
+export const handler = serverless(expressApp);
