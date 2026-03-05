6f2e6a4115ae151e06e83c2fa5de15c6b432ceee
MahmoudShaaban00 <ms3198@fayoum.edu.eg>
2026-03-06 01:37:37 +0200

    update project

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import  express from 'express';
-import  serverless from 'serverless-http';
+import express from 'express';
+import serverless from 'serverless-http';
+import cors from 'cors';
 
 const expressApp = express();
+
+// CORS هنا مهم جدا
+expressApp.use(
+  cors({
+    origin: "*",
+    methods: ["GET","HEAD","PUT","PATCH","POST","DELETE","OPTIONS"],
+    allowedHeaders: "*",
+  })
+);
+
 const adapter = new ExpressAdapter(expressApp);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, adapter);
-
-  // تفعيل CORS عشان الـ frontend يقدر يطلب من الـ backend
-app.enableCors({
-  origin: ['http://localhost:3000', 'https://your-frontend.vercel.app'], // رابط الـ frontend الحقيقي
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // مهم ل preflight
-  allowedHeaders: '*', // يسمح بأي header
-  credentials: true,
-});
-
   await app.init();
 }
 
